494142f1d54a5ef3e2b3b5266804075f19ebdc9b
Alex Xia <alex.xia@nokia-sbell.com>
2020-04-27 16:29:02 +0800

    update

--- otn_wa/controller.py
+++ otn_wa/controller.py
@@ -1,5 +1,4 @@
 #! /usr/bin/env python3
-# encoding: utf-8
 
 from flask import Flask, abort, request, jsonify
 from flask_cors import CORS
@@ -7,7 +6,8 @@ import os
 import json
 
 import alarm_names
-import pm_data_ftp
+import pm_data_ftp, alarm_data_ftp
+import enms_db
 
 app = Flask(__name__)
 CORS(app, resources=r'/*')
@@ -56,6 +56,28 @@ def pm_upload():
         request.json['remote_name']
     )
     
+@app.route('/alarm_upload', methods=['POST'])
+def alarm_upload():
+    if not request.json:
+        abort(500)
+    if 'ip' not in request.json \
+        or 'user' not in request.json \
+        or 'password' not in request.json \
+        or 'remote_location' not in request.json \
+        or 'remote_name' not in request.json \
+        or 'file' not in request.json:
+        abort(501)
+    print(request.json)
+    return alarm_data_ftp.alarm_upload(
+        request.json['ip'],
+        request.json['port'],
+        request.json['user'],
+        request.json['password'],
+        request.json['file'],
+        request.json['remote_location'],
+        request.json['remote_name']
+    )
+
 @app.route('/pm_download', methods=['POST'])
 def pm_download():
     if not request.json:
@@ -77,6 +99,49 @@ def pm_download():
         request.json['remote_name']
     )
 
+@app.route('/pm_tables', methods=['GET'])
+def pm_collections():
+    if not request.args or 'file' not in request.args:
+        abort(500)
+    return jsonify(enms_db.get_pm_collections())
+
+@app.route('/pm_table', methods=['GET'])
+def pm_collection():
+    if not request.args\
+        or 'file' not in request.args\
+        or 'table' not in request.args\
+        or 'page' not in request.args\
+        or 'page_size' not in request.args:
+        abort(500)
+    return jsonify(enms_db.get_pm_collection(
+        request.args['table'], 
+        request.args['file'],
+        int(request.args['page']),
+        int(request.args['page_size'])
+    ))
+
+@app.route('/alarm_tables', methods=['GET'])
+def alarm_collections():
+    if not request.args or 'file' not in request.args:
+        abort(500)
+    return jsonify(enms_db.get_alarm_collections())
+
+@app.route('/alarm_table', methods=['GET'])
+def alarm_collection():
+    if not request.args\
+        or 'file' not in request.args\
+        or 'table' not in request.args\
+        or 'page' not in request.args\
+        or 'page_size' not in request.args:
+        abort(500)
+    return jsonify(enms_db.get_alarm_collection(
+        request.args['table'], 
+        request.args['file'],
+        int(request.args['page']),
+        int(request.args['page_size'])
+    ))
+
+
 @app.route('/pm_files', methods=['GET'])
 def get_pm_files():
     return jsonify(pm_data_ftp.pm_file_list())
@@ -89,6 +154,17 @@ def get_pm_upload_logs():
 def get_pm_upload_logs_detail():
     return jsonify(pm_data_ftp.get_log())
 
+@app.route('/alarm_files', methods=['GET'])
+def get_alarm_files():
+    return jsonify(alarm_data_ftp.alarm_file_list())
+
+@app.route('/alarm_upload_logs', methods=['GET'])
+def get_alarm_upload_logs():
+    return jsonify(alarm_data_ftp.get_alarm_upload_log())
+
+@app.route('/alarm_upload_logs_detail', methods=['GET'])
+def get_alarm_upload_logs_detail():
+    return jsonify(alarm_data_ftp.get_log())
 
 @app.route('/alarm_names', methods=['PUT', 'GET'])
 def put_alarm_names():
---
+++ otn_wa/enms_db.py
@@ -0,0 +1,155 @@
+#! /usr/bin/env python3
+import pymongo
+import json
+import datetime
+
+db_host = 'mongodb://135.251.96.92:27017/'
+
+def init_target_db(db_name):
+    client = pymongo.MongoClient(db_host)
+    db_list = client.list_database_names()
+    if db_name in db_list:
+        return client[db_name], client
+    else:
+        client.close()
+        return None, None
+
+def get_pm_collections():
+    db, client = init_target_db('pm')
+    if db == None:
+        return []
+    client.close()
+    return db.list_collection_names()
+
+def get_alarm_collections():
+    return ["alarm_current", "alarm_history"]
+
+# translate "PM_20200415_163415@OTNE_1-19" to timestamp
+def get_timestamp(input_symbol):
+    result = 1586968455000 # 2020/04/15 16:34:15 UK
+    try:
+        date_str = input_symbol.split("_")[1]
+        time_str = input_symbol.split("_")[2]
+        year   = int(date_str[0:4])
+        month  = int(date_str[4:6])
+        day    = int(date_str[6:8])
+        hour   = int(time_str[0:2])
+        minute = int(time_str[2:4])
+        second = int(time_str[4:6])
+    except:
+        print("user default time: 1589560455000")
+        return result
+    t = int(datetime.datetime(year, month, day, hour, minute, second).timestamp()*1000)
+    # the symbol was recorded as UTC, there is 8-hour gap
+    return t + 8 * 3600 * 1000
+
+# make the json object the flat&paint line:
+def json_to_line(obj):
+    result = {}
+    for key in obj:
+        if isinstance(obj[key], datetime.datetime):
+            #result[key] = f"{obj[key].year}-{obj[key].month}-{obj[key].day} {obj[key].hour}:{obj[key].minute}:{obj[key].second}"
+            result[key] = obj[key].isoformat()
+        elif type(obj[key]) is dict:
+            append_dict(target_dict=result, prefix=key, source_dict=obj[key])
+        else:
+            result[key] = obj[key]
+    return result
+
+def append_dict(target_dict, prefix, source_dict):
+    for key in source_dict:
+        target_key = f"{prefix}/{key}"
+        if isinstance(source_dict[key], datetime.datetime):
+            target_dict[target_key] = source_dict[key].isoformat()
+        elif type(source_dict[key]) is dict:
+            append_dict(target_dict, target_key, source_dict[key])
+        else:
+            target_dict[target_key] = source_dict[key]
+
+'''
+return the collection documents like: {
+    "count": 129262,
+    "page": 1,
+    "page_size": 100
+    "data": [
+        {
+            "_id": "5e1ee3c632d0bb5c044625a3",
+            "tpid": "5e1dd6ad4b3d746ded862fae_17105920",
+            "tpUserLabel": "AHPLG-1-5-LINE",
+            "granularity": 0,
+            "bin": 1,
+            "endTimeEpoch": 1579139100000,
+            "endTime": "2020-01-16T01:45:00",
+            "neUserLabel": "201",
+            "neid": "5e1dd6ad4b3d746ded862fae",
+            "groups/tnOpInStat/sBinStatus": "dataComplete",
+            "groups/tnOpInStat/counters/MaxPower": "-1029",
+            "groups/tnOpInStat/counters/AveragePower": "-1032",
+            "groups/tnOpInStat/counters/MinPower": "-1034",
+            "groups/tnOpOutStat/sBinStatus": "dataComplete",
+            "groups/tnOpOutStat/counters/MaxPower": "-4000",
+            "groups/tnOpOutStat/counters/AveragePower": "-9990",
+            "groups/tnOpOutStat/counters/MinPower": "-4000",
+            "params/neNumber": 10002
+        }
+    ]
+}
+'''
+def get_pm_collection(collection_name, time_symbol=0, page=1, page_size=100):
+    timestamp = get_timestamp(time_symbol)
+    db, client = init_target_db('pm')
+    collection = db[collection_name]
+    f = {"endTimeEpoch": {'$lte': timestamp}}
+    total = collection.count_documents(f)
+    items = collection.find(f).skip((page-1)*page_size).limit(page_size)
+    result = {
+        "count": total,
+        "page": page,
+        "page_size": page_size,
+        "data": []
+    }
+    for raw_item in items:
+        # start to convert json-like objects to table-lines
+        item = json_to_line(raw_item)
+        result['data'].append(item)
+    pass
+    # close DB
+    client.close()
+    return result
+
+# Alarm_20200419_110606@OTNE_1-19
+def get_alarm_collection(table_name, time_symbol=0, page=1, page_size=20):
+    collection_name = table_name.replace("_", ".")
+    timestamp = get_timestamp(time_symbol)
+    db, client = init_target_db("controller_V2")
+    collection = db[collection_name]
+    f = {"nmsRaiseTime": {'$lte': timestamp}}
+    total = collection.count_documents(f)
+    items = collection.find(f).skip((page-1)*page_size).limit(page_size)
+    result = {
+        "count": total,
+        "page": page,
+        "page_size": page_size,
+        "data": []
+    }
+    for raw_item in items:
+        # start to convert json-like objects to table-lines
+        item = json_to_line(raw_item)
+        result['data'].append(item)
+    pass
+    # close DB
+    client.close()
+    return result    
+
+if __name__ == "__main__":
+    # let's try to read Pm_History_5e1dd6ad4b3d746ded862fae
+    # backup_file = "PM_20200415_163415@OTNE_1-19"
+    # print(get_timestamp(backup_file))
+    # collection_name = 'Pm_History_5e1dd6ad4b3d746ded862fae'
+    # result = get_pm_collection(collection_name, time_symbol=backup_file, page=2, page_size=1)
+    # print(json.dumps(result, ensure_ascii=False, indent=4))
+    backup_file = "Alarm_20200419_110606@OTNE_1-19"
+    print(get_timestamp(backup_file))
+    collection_name = 'alarm_current'
+    result = get_alarm_collection(collection_name, time_symbol=backup_file, page=2, page_size=2)
+    print(json.dumps(result, ensure_ascii=False, indent=4))
