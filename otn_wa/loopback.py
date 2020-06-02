f2b5791fb8e81cf80968cce8d4098916b4ca2639
Alex Xia <alex.xia@nokia-sbell.com>
2020-06-03 01:20:52 +0800

    add lookback test

---
+++ otn_wa/loopback.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python3
+import requests
+
+from util.litchi import Litchi as DB
+from remote_console.ssh_client import SSHClient 
+from remote_console.telnet_client import TelnetClient
+
+import config
+
+class Loopback:
+
+    def __init__(self, scheduler_ins):
+        self.db_name = 'loopback_release'
+        self.db_ins = DB(self.db_name)
+        scheduler_ins.register(self.db_name, self.db_ins, self.release_loopback)
+    
+    def login_NSP(self):
+        username = config.NSP_USERNAME
+        password = config.NSP_PASSWORD
+        url_init0 = f'{config.NSP_BASE_IP}'
+        url_login = f'{config.NSP_BASE_IP}/cas/login'
+        formdata = {
+            "username": username,
+            "password": password,
+            "_eventId": "submit",
+            "geolocation": ""
+        }
+        headers = { }
+        self.session = requests.session()
+        r = self.session.get(url_init0, verify=False)
+        execution = r.content.splitlines()[25][53:]
+        execution = execution[0: len(execution)-8]
+        formdata["execution"] = execution
+        r = self.session.post(url_login, data=formdata, verify=False, headers=headers)
+        pass
+
+    def send_release_loopback_via_NSP(self, item):
+        portId = item['portId']
+        id = portId
+        if item['type'] == 'Cmd_Facility_Loopback':
+            loopback_type = 'FACILITY'
+        else:
+            loopback_type = "TERMINAL"
+        layerRateId = item['layerRateId']
+        neId = item['neId']
+        tpDisplayLabel = item['tpDisplayLabel']
+        neName = item['neName']
+        # build url with parameters
+        # sample = '?loopbackCommandName=PerformRelease&portLoopbackType=FACILITY&layerRateId=20&neId=46&tpDisplayLabel=130SCX10-1-5-L1&portId=123564&neName=pss32-152'
+        base = f'{config.NSP_BASE_IP}:{config.NSP_PORT}/oms1350/data/otn/ports/{id}/loopback' 
+        param = f'?loopbackCommandName=PerformRelease&portLoopbackType={loopback_type}&layerRateId={layerRateId}&neId={neId}&tpDisplayLabel={tpDisplayLabel}&portId={portId}&neName={neName}'
+        url_release_loopback = base + param
+        print(url_release_loopback)
+        consistpm_req_body = { "Tag": "Cmd_Release_Loopback" }
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+        }
+        r = self.session.post(url_release_loopback, json=consistpm_req_body, verify=False, headers=headers)
+        print(r.text)
+        pass
+
+    def send_release_loopback_via_vcli(self, item):
+        ip = item["neIp"]
+        label = item["tpDisplayLabel"].split("-", 1)[1].replace('-', '/')
+        "aaaxxx".split()
+        client = SSHClient(ip, 'root', 'ALu12#')
+        if client is None: return -102
+        client.connect()
+        r = client.exec_batch([
+            ('vsim cli\n', 'Username: '),
+            ('admin\n', 'Password: '),
+            ('admin\n', '(Y/N)?'),
+            ('Y\n', "# "),
+            (f'config interface {label} loopback facility disable\n', "# "),
+            (f'config interface {label} loopback terminal disable\n', "# ")
+        ])
+        pass
+
+    def send_release_loopback_via_cli(self, item):
+        pass
+
+    def release_loopback(self, task):
+        print(task)
+        # self.login_NSP()
+        for item in task['items']:
+            self.send_release_loopback_via_vcli(item)
+
+        self.db_ins.delete_item_by_id(str(task['id']))
+        
+    def add_loopback_release_task(self, task):
+        return self.db_ins.add_item(task)
--- otn_wa/remote_console/ssh_client.py
+++ otn_wa/remote_console/ssh_client.py
@@ -1,26 +1,78 @@
 import paramiko
 
 class SSHClient(object):
-    def __init__(self, hostname, port, username, password):
+    def __init__(self, hostname, username, password, port=22):
         self.hostname = hostname
         self.port = port
         self.username = username
         self.password = password
+        self.client = None
         try:
             self.client = paramiko.SSHClient()
+            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         except Exception as e:
             print("Exception: %s: %s" % (e.__class__, e))
-
+            self.client = None
+    
+    def connect(self, expected='# '):
+        """
+        连接开始.
+        这里的expected是登录成功之后的提示符. 
+        强烈建议不要用默认值, 而是根据登录的系统的实际情况, 输入实当的参数.
+        """
+        self.client.connect(self.hostname, username=self.username, password=self.password)
+        self.chan = self.client.invoke_shell()
+        buff = ''
+        while not buff.endswith(expected):
+            resp = self.chan.recv(9999)
+            buff += resp.decode()
+        print(buff)
+        return 1
 
     def close(self):
         self.client.close()
 
-    def connect(self):
-        return self.client.connet(self.hostname, self.port, self.username, self.password)
+    def exec_batch(self, command_list):
+        """
+        执行一个序列的多个命令
+        """
+        for command in command_list:
+            if len(command) == 2:
+                r = self.exec(command[0], command[1])
+            elif len(command) == 3:
+                r = self.exec(command[0], command[1], error_message=command[2])
+            else:
+                r = -2
+            if r < 0:
+                return r
+        return 1
 
-    def exec(self, command):
-        pass
+    def exec(self, command, expected, error_message="这是全宇宙不可能出现的字符串"):
+        """
+        执行一个命令
+        command是输入的命令, 请注意回车也是命令的一部分, 需要适当加入'\n', 否则没有效果.
+        """
+        self.chan.send(command)
+        buff = ''
+        while not buff.endswith(expected):
+            resp = self.chan.recv(9999)
+            buff += resp.decode()
+            print(buff)
+            if error_message in buff:
+                return -1
+        return 1
 
-
+if __name__ == "__main__":
+    client = SSHClient("172.24.166.141", 'root', 'ALu12#')
+    if client is None: exit()
+    client.connect()
+    # 小心空格千万不能省略
+    r = client.exec_batch([
+        ('vsim cli\n', 'Username: '),
+        ('admin\n', 'Password: '),
+        ('admin\n', '(Y/N)?'),
+        ('Y\n', "# ")
+    ])
+    client.close()
 
 
